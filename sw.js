487f84c305f0f69575cc2bc1c14e6b47521e5fd0
yashsagadevan2007 <yashsagadevan2007@gmail.com>
2026-03-31 21:48:17 +0530

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("astar-cache").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
